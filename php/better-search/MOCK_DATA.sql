bac67e6915be59b4449e0632c4a4433122d6e0cb
comphonia <37687869+comphonia@users.noreply.github.com>
2018-12-01 18:11:13 -0600

    Create MOCK_DATA.sql
    Run the sql commands to add table and data to database.

---
+++ php/better-search/MOCK_DATA.sql
@@ -0,0 +1,31 @@
+create table MOCK_DATA (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50)
+);
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (1, 'Worthy', 'Ourtic', 'wourtic0@merriam-webster.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (2, 'Giuseppe', 'Pacquet', 'gpacquet1@furl.net', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (3, 'Guendolen', 'Borland', 'gborland2@reuters.com', 'Female');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (4, 'Winona', 'Troup', 'wtroup3@nyu.edu', 'Female');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (5, 'Emelyne', 'Braunle', 'ebraunle4@g.co', 'Female');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (6, 'Otto', 'Antosch', 'oantosch5@hhs.gov', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (7, 'Beck', 'Doidge', 'bdoidge6@cnbc.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (8, 'Sebastien', 'Lawford', 'slawford7@mtv.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (9, 'Alejoa', 'Labone', 'alabone8@163.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (10, 'Janos', 'Bagehot', 'jbagehot9@elpais.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (11, 'Shelba', 'Kee', 'skeea@taobao.com', 'Female');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (12, 'Sanderson', 'Stirton', 'sstirtonb@hibu.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (13, 'Montague', 'Agglio', 'magglioc@tumblr.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (14, 'Torey', 'Hambright', 'thambrightd@simplemachines.org', 'Female');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (15, 'Kristy', 'Bramwich', 'kbramwiche@google.de', 'Female');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (16, 'Horace', 'Witherspoon', 'hwitherspoonf@usa.gov', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (17, 'Maribel', 'Cahan', 'mcahang@cargocollective.com', 'Female');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (18, 'Lind', 'Pechan', 'lpechanh@macromedia.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (19, 'Jarret', 'Rosencwaig', 'jrosencwaigi@spiegel.de', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (20, 'Edgardo', 'Iglesia', 'eiglesiaj@whitehouse.gov', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (21, 'Olly', 'Minchenton', 'ominchentonk@discuz.net', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (22, 'Ken', 'Reeve', 'kreevel@opera.com', 'Male');
+insert into MOCK_DATA (id, first_name, last_name, email, gender) values (23, 'Fielding', 'Minet', 'fminetm@wikia.com', 'Male');
+
